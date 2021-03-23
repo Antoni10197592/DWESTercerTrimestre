4e3687c71ec0c64bfd802168d6cfc573af4470f4
Antoni10197592 <alufol592@ieselcaminas.org>
2021-03-23 12:03:08 +0100

    tabla seca

--- prestamos/lib/utils.php
+++ prestamos/lib/utils.php
@@ -1,10 +1,30 @@
 
 <?php
 function get_prestamos(){
-	$txt=file_get_contents('data/prestamos.txt', false);
-	return $txt;
+	$contenido=file_get_contents('data/prestamos.txt', false);
+	$filas=explode("\n",$contenido);
+	$resultado= "<table class=\"tabla\">
+		<thead>
+			<tr>
+				<th>Usuario</th>
+				<th>Tipo</th>
+				<th>Nombre</th>
+				<th>Fecha</th>
+			</tr>
+		</thead>
+		<tbody>";
+
+		foreach($filas as $linea){
+			$columna= explode("\t", $linea);
+			$resultado .="<tr>";
+			$resultado .="<td>".$columna[0]."</td>";
+			$resultado .="<td>".$columna[1]."</td>";
+			$resultado .="<td>".$columna[2]."</td>";
+			$resultado .="<td>".$columna[3]."</td>";
+			$resultado.="</tr>";
+		}
+		$resultado.="</tbody></table>";
+
+	return $resultado;
 }
-
-
-
 ?>
