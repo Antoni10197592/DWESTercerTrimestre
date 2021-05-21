10209eb93342865d9d3bb59667a0be5a10d77c3c
Antoni10197592 <alufol592@ieselcaminas.org>
2021-05-21 09:51:44 +0200

    Casi finalizado al completo, falta editar.

--- prestamos/lib/borrar.php
+++ prestamos/lib/borrar.php
@@ -6,8 +6,10 @@ function borrar_pedido($id){
 	$conexion = conexion_bd();
 	$sql="DELETE FROM Prestamos WHERE ID_pedido=$id";
 	$sentencia = mysqli_query($conexion, $sql);
+	
 }
-
+borrar_pedido($id);
+header("Location: ../index.php");
 ?>
 
 
--- prestamos/lib/editar.php
+++ prestamos/lib/editar.php
@@ -1,6 +1,19 @@
-<?php
+<?php require_once 'utils.php';
 
-function editar_pedido(){
+	$id=$_GET['ID_pedido'];
+	$tipo=$_GET['type'];
+	$fecha=$_GET['date'];
+	$titulo=$_GET['title'];
 
-}
+	function editar_pedido($id){
+		$conexion = conexion_bd();
+		$sql="UPDATE Prestamos 
+			SET Tipo="$tipo" , Fecha="$fecha", Descripcion="$titulo"
+			WHERE ID_pedido=$id";
+		$sentencia = mysqli_query($conexion, $sql);	
+
+	}
+
+	editar_pedido($id);
+	header("Location: ../index.php");
 ?>
